458dbf3b0e89633df8c37fc94d78aa6ac3398732
Drew Kang <kangdrew95@gmail.com>
2021-08-18 20:24:46 +0000

    Added some queries

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,29 @@
+-- My version, didn't incldue North Vancouver...
+
+-- SELECT properties.id,
+--   properties.title,
+--   properties.cost_per_night,
+--   AVG(property_reviews.rating) AS average_rating
+-- FROM properties
+-- JOIN property_reviews ON properties.id = property_reviews.property_id
+-- WHERE city = 'Vancouver'
+-- GROUP BY properties.id
+-- HAVING AVG(property_reviews.rating) >= 4
+-- ORDER BY cost_per_night
+-- LIMIT 10;
+
+
+-- Actual solution...
+
+SELECT properties.id,
+  properties.title,
+  properties.cost_per_night,
+  properties.city,
+  avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
